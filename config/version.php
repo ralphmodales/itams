2d6d69e792ef78a484e0c0a1927f5c805ba09fe1
ralphmodales <ralphmodales@gmail.com>
2024-10-01 14:07:33 +0800

    first commit

---
+++ config/version.php
@@ -0,0 +1,10 @@
+<?php
+return array (
+  'app_version' => 'v6.1.1',
+  'full_app_version' => 'v6.1.1 - build 10847-g2ac4449ea',
+  'build_version' => '10847',
+  'prerelease_version' => '',
+  'hash_version' => 'g2ac4449ea',
+  'full_hash' => 'v6.1.1-605-g2ac4449ea',
+  'branch' => 'master',
+);
