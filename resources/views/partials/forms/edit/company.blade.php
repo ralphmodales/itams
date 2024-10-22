1667c1b1761a96b7dac8426bda41eaca61959afb
ralphmodales <ralphmodales@gmail.com>
2024-10-22 12:58:36 +0800

    Bug fixed

--- resources/views/partials/forms/edit/company.blade.php
+++ resources/views/partials/forms/edit/company.blade.php
@@ -1,7 +1,7 @@
 @if (\App\Models\Company::isCurrentUserAuthorized())
 <div class="form-group {{ $errors->has('company_id') ? ' has-error' : '' }}">
    <div class="col-md-3 control-label">
-       {{ Form::label('company_id', 'Business Unit', array('class' => 'col-md-3 control-label', 'for' => 'company_id')) }}
+       {{ Form::label('company_id', trans('general.company'), array('class' => 'col-md-3 control-label', 'for' => 'company_id')) }}
    </div>
     <div class="col-md-7 col-sm-12{{  (Helper::checkIfRequired($item, 'company_id')) ? ' required' : '' }}">
        {{ Form::select('company_id', $company_list , old('company_id', $item->company_id), array('class'=>'select2', 'style'=>'width:100%')) }}
